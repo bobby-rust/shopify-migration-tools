2515f29e4f106c1f60939ebe02b8766465f6c415
Rob <bobby.rust121@gmail.com>
2024-12-20 20:16:06 -0500

    error log formatting

--- blog_uploader.py
+++ blog_uploader.py
@@ -152,11 +152,11 @@ def create_article(blog_id, author, title, content, published_at, updated_at,
     else:
         error = response.json()
         with open("errors.txt", "a") as error_log:
-            error_log.write("----------------------")
+            error_log.write("----------------------\n")
             error_log.write(f"Failed to create blog post {title}: ")
             error_log.write(str(error))
-            error_log.write("----------------------")
-            error_log.write("\n\n")
+            error_log.write("----------------------\n")
+            error_log.write("\n")
             
     return response.json()
 
